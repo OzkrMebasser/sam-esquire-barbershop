fc33a4e64d3e83b33f92a5b9f8d6053325143310
OzkrMebasser <oscarmorenoestudios@gmail.com>
2023-08-12 00:42:00 -0700

    feat: adding gallery and filters

---
+++ assets/js/open-hours-w-close-now.js
@@ -0,0 +1,94 @@
+(function (window) {
+    "use strict";
+  
+    function define_openHours() {
+  
+      let OpenHours = {};
+  
+      let insertColon = function (time) {
+        let center = time.length - 2;
+        let timeWithColon = [time.slice(0, center), ":", time.slice(center)].join("");
+        return timeWithColon;
+      };
+  
+      let formatTime = function (time) {
+        if (!time) {
+          return "Closed"; 
+        } else {
+          if (time >= 1200) {
+            var formattedTime = time === 1200 ? 1200 : time - 1200;
+            return `${insertColon(formattedTime.toString())} PM`;
+          } else {
+            return `${insertColon(time.toString())} AM`;
+          }
+        }
+      };
+  
+      const weekIndex = {
+        0: "Sunday",
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+      };
+  
+      let determineDay = function (day, dayDiv) {
+        
+        let now = new Date();
+  
+        if (day === weekIndex[now.getDay()]) {
+          // Styling for today
+        } else {
+          dayDiv.style["font-weight"] = "normal";
+          dayDiv.style["color"] = "black"; 
+          dayDiv.style["text-decoration"] = "none";
+        }
+  
+      };
+  
+      OpenHours["generateTime"] = function (hours) {
+        
+        let rootElement = document.getElementById("open-hours")
+        let table = document.createElement("table")
+        if (rootElement) {
+          for (let day in hours) {
+            let dayDiv = document.createElement("tr")
+  
+            determineDay(day, dayDiv)
+  
+            let dayTitleElement = document.createElement("td")
+            let dayTimesElement = document.createElement("td")
+  
+            let dayTitle = document.createTextNode(day)
+            dayTitleElement.appendChild(dayTitle)
+            dayDiv.appendChild(dayTitleElement)
+  
+            let dayHours = ""
+  
+            if (Object.keys(hours[day]).length === 0) {
+              dayHours = `Closed`
+            } else {
+              dayHours = `${formatTime(hours[day]["start"])} - ${formatTime(hours[day]["end"])}`
+            }
+  
+            let dayTimes = document.createTextNode(dayHours)
+            dayTimesElement.appendChild(dayTimes)
+            dayDiv.appendChild(dayTimesElement)
+            table.appendChild(dayDiv)
+          }
+          rootElement.appendChild(table)
+        }
+      }
+  
+  
+      return OpenHours;
+  
+    }
+  
+    if (typeof OpenHours === "undefined") {
+      window.OpenHours = define_openHours();
+    }
+  
+  })(window);
