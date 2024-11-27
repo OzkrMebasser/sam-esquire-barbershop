28468bde3c0dcfce2cfbb723060dcc78bbd221d6
OzkrMebasser <oscarmorenoestudios@gmail.com>
2024-11-26 23:25:09 -0800

    Setting up everything & fixing bug for re-deploy

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,10 +1,11 @@
-const modal= document.querySelector(".overlay");
+const modal = document.querySelector(".overlay");
 
-setTimeout(function() { 
+setTimeout(function() {
   modal.classList.add("show-modal");
 }, 1000);
 
 document.querySelector("#close-modal-btn").addEventListener("click", () => {
   modal.classList.remove("show-modal");
   modal.classList.add("hide-modal");
-})
+  
+});
